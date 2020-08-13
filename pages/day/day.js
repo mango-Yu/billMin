09086c8c7cd4369c2576f2426557f27f7d86d201
yuyi.shi <yuyi.shi@scienjoy.com>
2020-08-13 21:03:12 +0800

    日消费修改

--- pages/day/day.js
+++ pages/day/day.js
@@ -16,37 +16,39 @@ Component({
    */
   data: {
     show: false, 
-    date: '',
-    breakfast: '',
-    lunch: '',
-    dinner: '',
-    traffic: '',
-    sock: '',
-    house: '',
-    work: true,
-    clothes: '',
-    clothesRemind: "",
-    play: '',
-    playRemind: "",
-    others: '',
-    othersRemind: "",
-    gifts: '',
-    giftsRemind: "",
-    buy: '',
-    buysRemind: "",
-    loans: '',
-    loansRemind: "",
-    visa: '',
-    visaRemind: "",
-    foods: '',
-    foodsRemind: "",
-    skin: '',
-    skinRemind: "",
-    health: '',
-    healthRemind: "",
-    insure: '',
-    insureRemind: "",
-    user: "" ,
+    form:{
+      date: '',
+      breakfast: '',
+      lunch: '',
+      dinner: '',
+      traffic: '',
+      sock: '',
+      house: '',
+      work: true,
+      clothes: '',
+      clothesRemind: "",
+      play: '',
+      playRemind: "",
+      others: '',
+      othersRemind: "",
+      gifts: '',
+      giftsRemind: "",
+      buy: '',
+      buysRemind: "",
+      loans: '',
+      loansRemind: "",
+      visa: '',
+      visaRemind: "",
+      foods: '',
+      foodsRemind: "",
+      skin: '',
+      skinRemind: "",
+      health: '',
+      healthRemind: "",
+      insure: '',
+      insureRemind: "",
+      user: "" ,
+    },
     now: "",
     currentDate: new Date().getTime(),
     minDate: new Date().getTime(),
@@ -62,15 +64,15 @@ Component({
   },
   computed: {
     eat(data){
-      let eatSum = (utils.funcDealNull(data.breakfast) + utils.funcDealNull(data.lunch) + utils.funcDealNull(data.dinner));
+      let eatSum = (utils.funcDealNull(data.form.breakfast) + utils.funcDealNull(data.form.lunch) + utils.funcDealNull(data.form.dinner));
       let eatTotal = isNaN(eatSum) ? 0 : eatSum.toFixed(2);
         return '吃饭费用（总计：' + eatTotal + '）'
     },
     all(data) {
-      let allSpend = (utils.funcDealNull(data.breakfast) + utils.funcDealNull(data.lunch) + utils.funcDealNull(data.dinner)
-                     + utils.funcDealNull(data.traffic) + utils.funcDealNull(data.sock) + utils.funcDealNull(data.clothes) + utils.funcDealNull(data.play)
-                     + utils.funcDealNull(data.others) + utils.funcDealNull(data.gifts) + utils.funcDealNull(data.buy) + utils.funcDealNull(data.foods)
-                     + utils.funcDealNull(data.visa) + utils.funcDealNull(data.loans) + utils.funcDealNull(data.skin) + utils.funcDealNull(data.health) + utils.funcDealNull(data.insure));
+      let allSpend = (utils.funcDealNull(data.form.breakfast) + utils.funcDealNull(data.form.lunch) + utils.funcDealNull(data.form.dinner)
+                     + utils.funcDealNull(data.form.traffic) + utils.funcDealNull(data.form.sock) + utils.funcDealNull(data.form.clothes) + utils.funcDealNull(data.form.play)
+                     + utils.funcDealNull(data.form.others) + utils.funcDealNull(data.form.gifts) + utils.funcDealNull(data.form.buy) + utils.funcDealNull(data.form.foods)
+                     + utils.funcDealNull(data.form.visa) + utils.funcDealNull(data.form.loans) + utils.funcDealNull(data.form.skin) + utils.funcDealNull(data.form.health) + utils.funcDealNull(data.form.insure));
        return isNaN(allSpend) ? 0 : allSpend.toFixed(2);
      }
   },
@@ -80,7 +82,10 @@ Component({
   methods: {
     onLoad(){
       this.setData({
-        date: utils.formatTime(new Date()),
+        form:{
+          date: utils.formatTime(new Date()),
+          user: wx.getStorageSync('name')
+        },
         now: utils.formatTime(new Date())
       });
       this.funcGetDay(utils.formatTime(new Date()))
@@ -100,12 +105,16 @@ Component({
       let selcetTime = utils.formatTime(dateTime);
       let that = this;
       this.setData({
-        date: utils.formatTime(dateTime),
+        form:{
+          date: utils.formatTime(dateTime),
+        },
         show: false
       });
       if (selcetTime < '2020-01-01') {
         this.setData({
-          date: '2020-01-01'
+          form:{
+            date: '2020-01-01'
+          }
         });
         wx.showToast({
           title: '2020-01-01是最早时间',
@@ -115,7 +124,9 @@ Component({
       }
       if (selcetTime > that.data.now) {
         this.setData({
-          date: that.data.now
+          form: {
+            date: that.data.now
+          }
         });
         wx.showToast({
           title: that.data.now + '是最晚时间',
@@ -134,69 +145,90 @@ Component({
     funcSetData(type, formData){
       if (type === 2) {
         this.setData({
-          breakfast: 0,
-          lunch: 0,
-          dinner: 0,
-          traffic: 0,
-          sock: 0,
-          house: 0,
-          work: true,
-          clothes: 0,
-          clothesRemind: '',
-          play: 0,
-          playRemind: '',
-          others: 0,
-          othersRemind: '',
-          gifts: 0,
-          giftsRemind: '',
-          buy: 0,
-          buysRemind: '',
-          loans: 0,
-          loansRemind: '',
-          visa: 0,
-          visaRemind: '',
-          foods: 0,
-          foodsRemind: '',
-          skin: 0,
-          skinRemind: '',
-          health: 0,
-          healthRemind: '',
-          insure: 0,
-          insureRemind: ''
+          form:{
+            date: that.data.form.date,
+            breakfast: 0,
+            lunch: 0,
+            dinner: 0,
+            traffic: 0,
+            sock: 0,
+            house: 0,
+            work: true,
+            clothes: 0,
+            clothesRemind: '',
+            play: 0,
+            playRemind: '',
+            others: 0,
+            othersRemind: '',
+            gifts: 0,
+            giftsRemind: '',
+            buy: 0,
+            buysRemind: '',
+            loans: 0,
+            loansRemind: '',
+            visa: 0,
+            visaRemind: '',
+            foods: 0,
+            foodsRemind: '',
+            skin: 0,
+            skinRemind: '',
+            health: 0,
+            healthRemind: '',
+            insure: 0,
+            insureRemind: ''
+          }
         })
       }else if (type === 1){
         this.setData({
-          breakfast: formData.breakfast,
-          lunch: formData.lunch,
-          dinner: formData.dinner,
-          traffic: formData.traffic,
-          sock: formData.sock,
-          house: formData.house,
-          work: formData.work ? true : false,
-          clothes: formData.clothes,
-          clothesRemind: formData.clothesRemind,
-          play: formData.play,
-          playRemind: formData.playRemind,
-          others: formData.others,
-          othersRemind: formData.othersRemind,
-          gifts: formData.gifts,
-          giftsRemind: formData.giftsRemind,
-          buy: formData.buy,
-          buysRemind: formData.buysRemind,
-          loans: formData.loans,
-          loansRemind: formData.loansRemind,
-          visa: formData.visa,
-          visaRemind: formData.visaRemind,
-          foods: formData.foods,
-          foodsRemind: formData.foodsRemind,
-          skin: formData.skin,
-          skinRemind: formData.skinRemind,
-          health: formData.health,
-          healthRemind: formData.healthRemind,
-          insure: formData.insure,
-          insureRemind: formData.insureRemind
+          form: {
+            date: utils.formatTime(new Date(formData.date)),
+            breakfast: formData.breakfast,
+            lunch: formData.lunch,
+            dinner: formData.dinner,
+            traffic: formData.traffic,
+            sock: formData.sock,
+            house: formData.house,
+            work: formData.work ? true : false,
+            clothes: formData.clothes,
+            clothesRemind: formData.clothesRemind,
+            play: formData.play,
+            playRemind: formData.playRemind,
+            others: formData.others,
+            othersRemind: formData.othersRemind,
+            gifts: formData.gifts,
+            giftsRemind: formData.giftsRemind,
+            buy: formData.buy,
+            buysRemind: formData.buysRemind,
+            loans: formData.loans,
+            loansRemind: formData.loansRemind,
+            visa: formData.visa,
+            visaRemind: formData.visaRemind,
+            foods: formData.foods,
+            foodsRemind: formData.foodsRemind,
+            skin: formData.skin,
+            skinRemind: formData.skinRemind,
+            health: formData.health,
+            healthRemind: formData.healthRemind,
+            insure: formData.insure,
+            insureRemind: formData.insureRemind
+          }
         })
       }
+    },
+    blurInput(e) {
+      console.log(e)
+      // let that = this;
+      // let num = this.data.form[val] + "";
+      // console.log(num)
+      // num = num.replace(/[^\-?\d.]/g, '');
+      // if (num == "" || num < 0) {
+      //   num = 0
+      // }
+      // this.setData({
+      //   form:{
+      //     val: num
+      //   }
+      // })
     },
     funcGetDay(d){
       let obj = {date:d}
@@ -226,7 +258,9 @@ Component({
                         that.funcSetData(2);
                 }else if (i == "work") {
                   that.setData({
-                    work: true
+                    form: {
+                      work: true
+                    }
                   });
                 }else {
                   if (i != "date" && i != "user" && i !== "work") {
@@ -247,7 +281,54 @@ Component({
       })
     },
     onSubmitDay(){
-      
+      var obj = this.data.form;
+      for (let i in obj) {
+        if (!(i == "playRemind" || i == "clothesRemind" || i == "othersRemind" || i == "giftsRemind" ||
+              i == "buysRemind" || i == "loansRemind" || i == "visaRemind" || i == "foodsRemind" || i == "skinRemind" || i == "healthRemind"|| i == "insureRemind"
+              || i == "date" || i == 'user')) {
+          if ((obj[i] + "") == "") {
+            obj[i] = isNaN(parseFloat(obj[i])) ? 0 : parseFloat(obj[i])
+          }
+        }
+      }
+      var that = this;
+      console.log(obj)
+      wx.request({
+        url:  `${config.api + '/subSpending'}`,
+        data: obj,
+        header: {
+          'content-type': 'application/json', // 默认值
+          'cookie': wx.getStorageSync("sessionid")
+          //读取sessionid,当作cookie传入后台将PHPSESSID做session_id使用
+        },
+        method: 'POST',
+        dataType: "json",
+        success: function (res) {
+          data = data.data
+          if (data.code == 1) {
+            wx.showToast({
+              title: '提交成功',
+              icon: 'success',
+              duration: 2000
+            })
+            this.funcGetDay(utils.formatTime(that.data.date))
+          } else {
+            wx.showToast({
+              title: '提交失败',
+              image: '../../images/fail.png',
+              duration: 2000
+            })
+          }
+        },
+        fail: function(err) {
+          console.log(err)
+          wx.showToast({
+            title: '提交失败',
+            image: '../../images/fail.png',
+            duration: 2000
+          })
+        }
+      })
     }
   }
 })
